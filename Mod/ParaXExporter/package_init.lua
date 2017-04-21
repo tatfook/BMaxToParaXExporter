6cc41c3d04428e7e7fd5ab34371efb71012d9d6c
LiXizhi <lixizhi@yeah.net>
2017-04-21 22:16:07 +0800

    package.npl added

---
+++ Mod/ParaXExporter/package_init.lua
@@ -0,0 +1,5 @@
+
+log(NPL.filename().." loaded\n")
+-- NPL.load("(gl)script/apps/Aries/Creator/Game/Mod/ModManager.lua");
+--local ModManager = commonlib.gettable("Mod.ModManager");
+--ModManager:GetLoader():AddSystemModule("ParaXExporter", {displayName = "ParaX 3D模型导出"});
